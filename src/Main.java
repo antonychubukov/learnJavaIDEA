85df9ff6e3c73933a2274c1cebd745d6ebb07acb
Anton <krdmasterweb@yandex.ru>
2025-01-06 22:22:36 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String[] args) {
+        String name = "Anton";
+        String surname = "Chubukov";
+        int age = 40;
+        printRes(name, surname, age);
+        a a = new a();
+        System.out.println(a);
+
+
+    }
+
+
+    static void printRes(String a, String b, int c){
+        String res = a+";"+b+";"+c;
+                System.out.println(res);
+    }
+}
--- src/anton/a.java
+++ src/anton/a.java
@@ -1,3 +1,5 @@
+package anton
+
 public class a {
     public static void main(String[] args) {
         System.out.println("o privet");
